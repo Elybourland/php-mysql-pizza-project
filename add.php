c036feb28aa5df3c687e9de63c5279dc5f485175
Ely Bourland <JEBourland@yahoo.com>
2020-12-05 14:20:52 -0800

    basic form validation for add.php

--- add.php
+++ add.php
@@ -11,10 +11,32 @@
     // $_POST request (more secure/hidden)
 
     if(isset($_POST['submit'])) {
-        echo htmlspecialchars($_POST['email']);
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']);
-    }
+
+        // check email
+
+        if(empty($_POST['email'])) {
+            echo 'An email is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['email']);
+        }
+
+        // check title
+
+        if(empty($_POST['title'])) {
+            echo 'A pizza title is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['title']);
+        }
+
+        // check ingredients
+
+        if(empty($_POST['ingredients'])) {
+            echo 'At least one ingredient is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['ingredients']);
+        }
+
+     } // end of POST check
 
 ?>
 
