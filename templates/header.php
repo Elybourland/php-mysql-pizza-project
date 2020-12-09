2c980160a0535b655304a1be77d033594b036e21
Ely Bourland <JEBourland@yahoo.com>
2020-12-08 18:21:04 -0800

    null coalescing with ??

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,27 @@
+<?php 
+
+
+    session_start();
+
+    // $_SESSION['name'] = 'yoshi';
+
+    if($_SERVER['QUERY_STRING'] == 'noname') {
+
+        unset($_SESSION['name']);
+
+        // session_unset();
+
+    }
+
+    // Null coalescing with ??
+
+    $name = $_SESSION['name'] ?? 'Guest';
+
+
+?>
+
+
+
 <head>
     <title>Ninja Pizza</title>
     <!-- Compiled and minified CSS -->
@@ -36,6 +60,7 @@
     <div class="container">
         <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
         <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
             <li><a href="add.php" class="btn brand z-depth-0">Add A Pizza</a></li>
         </ul>
     </div>
